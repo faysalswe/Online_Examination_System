992a4ddfe84981be341865622c5fa0534fe30ea4
Faysal Ahmmad <faysal725@diu.edu.bd>
2018-03-06 03:14:46 +0600

    question.json add as a datasource for student question

--- src/app/shared-imports.module.ts
+++ src/app/shared-imports.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   imports: [
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    NgxPaginationModule
   ],
   exports: [
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    NgxPaginationModule
   ]
 })
 export class SharedImportsModule { }
